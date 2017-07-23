b1b9abf92d74effa0640a72a99de1a0c0e5d8fc4
YFdyh000 <yfdyh000@gmail.com>
2017-07-23 08:41:56 +0800

    Avoid using lodash in main to reduce package size

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="globals/async/index.d.ts" />
 /// <reference path="globals/css/index.d.ts" />
 /// <reference path="globals/less/index.d.ts" />
-/// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/source-map/index.d.ts" />
